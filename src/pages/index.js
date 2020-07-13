8fd6f173a242c0d190ef45c2e3d830c7cd02ea7d
dllaforce <61428170+dllaforce@users.noreply.github.com>
2020-07-13 12:54:28 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,13 @@
 import React from 'react'
-import get from 'lodash/get'
-import {Image, Header} from 'semantic-ui-react'
 import SEO from '../components/SEO'
-import logo from '../images/ill-short-dark.svg'
 import Layout from '../components/Layout'
 
-  const siteTitle = get(data, 'site.siteMetadata.title')
-  const Home = ({location}) => (
+const Home = ({location}) => (
     <Layout location={location}>
-      <SEO title={siteTitle} />
-      <Header
-        as="h3"
-        icon
-        textAlign="center"
-        style={{
-          marginBottom: '2em',
-        }}
-      >
-        <Header.Content
-          style={{
-            width: '60%',
-            margin: '0 auto',
-          }}
-        >
-          <Image src={logo} alt="logo" />
-        </Header.Content>
-      </Header>
-      Test Body
+    <SEO title="Home" />
+    <h1>Home</h1>
+      <p>Welcome Home</p>
     </Layout>
 )
     
-export default StoreIndex
+export default Home
